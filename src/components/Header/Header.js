82bfcc3f10202aae63749375b7097dcba724b88e
Asif10H <habibullahasif71@gmail.com>
2021-03-11 15:37:52 +0600

    added stadium img section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import head from '../photo/head.png'
 import './Header.css'
 const Header = () => {
     return (
-        <div className="card bg-dark text-white img-responsive">
-            <img src={head} className="card-img" alt="heading" />    
+        <div className="heading-section card border-0   text-white img-responsive text-center">
+            <h1 className="heading-section">Football Clubs</h1>
         </div>
     );
 };
--- src/components/TeamDetail/TeamDetail.js
+++ src/components/TeamDetail/TeamDetail.js
@@ -1,57 +1,70 @@
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FaFacebook, FaTwitter, FaInstagram, FaFirefox } from 'react-icons/fa';
-import {BiWorld} from 'react-icons/bi'; 
-import {FiFlag} from 'react-icons/fi'; 
-import {RiFootballLine} from 'react-icons/ri'; 
-import {CgGenderMale} from 'react-icons/cg';
+import { BiWorld } from 'react-icons/bi';
+import { FiFlag } from 'react-icons/fi';
+import { RiFootballLine } from 'react-icons/ri';
+import { CgGenderMale } from 'react-icons/cg';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import male from '../photo/male.png'
 import female from '../photo/female.png'
 import './TeamDetail.css';
+import head from '../photo/head.png'
 
 const TeamDetail = () => {
     const { idTeam } = useParams()
     const [team, setTeam] = useState([]);
 
     const { strTeam, strTeamBadge, intFormedYear, strCountry, strGender, strDescriptionEN, strDescriptionES,
-        strWebsite, strFacebook, strTwitter, strInstagram } = team;
+        strWebsite, strFacebook, strTwitter, strInstagram, strStadiumThumb } = team;
 
     useEffect(() => {
         fetch(`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`)
             .then(res => res.json())
             .then(data => setTeam(data.teams[0]))
     }, [idTeam])
-
+    // male or female club checked
     const gender = strGender;
-    let playersTeam;
+    let playersTeam = '';
     if (gender === "Male") {
-        playersTeam = <img className="img-fluid" src={male} alt="football team" />
+        playersTeam = <img src={male} alt="football team" />
     }
     else {
-        playersTeam = <img className="img-fluid" src={female} alt="football team" />
+        playersTeam = <img src={female} alt="football team" />
+    }
+    // empty stadium picture checked
+    const stadiumThumb = strStadiumThumb;
+    let clubsStadium;
+    if (stadiumThumb === null) {
+        clubsStadium = <img src={head} className="card-img stadium-thumb" alt="stadium" />
+    }
+    else {
+        clubsStadium = <img src={strStadiumThumb} className="card-img stadium-thumb" alt="stadium" />
     }
     return (
         <div className="container">
-            <div className="row">
-                <div className="col-lg-4 col-md-6 col-sm-12 center">
-                    <img className="d-block center" src={strTeamBadge} alt="Team Badge" />
+            <div className="container wrapper stadium-badge col-lg-4 col-md-6 col-sm-12 center">
+                <div className="card bg-dark text-white img-responsive">
+                    {clubsStadium}
+                </div>
+                <div>
+                    <img className="d-block center play" src={strTeamBadge} alt="Team Badge" />
                 </div>
             </div>
             <div className="container bg-primary text-white rounded m-1">
-                <div className="container row justify-content-between">
+                <div className="row justify-content-between">
                     <div className="col-lg-4 col-md-6 col-sm-12">
-                        <h1 className="card-title">{strTeam}</h1> 
-                        <h4 className="card-text"> <BiWorld style={{ height: 25, width: 25 }} /> Founded: {intFormedYear}</h4>
-                        <h4 className="card-text"> <FiFlag style={{  height: 25, width: 25 }} /> Country: {strCountry}</h4>
-                        <h4 className="card-text"><RiFootballLine style={{ height: 25, width: 25 }} /> Sport Type: Football</h4>
-                        <h4 className="card-text"><CgGenderMale style={{  height: 40, width: 40 }} /> Gender: {strGender}</h4>
-                        <Link className="btn btn-dark" to={"/home"}><FontAwesomeIcon icon={faArrowLeft} /> Back </Link>
-                    </div>
-                    <div className=" col-lg-4 col-md-6 col-sm-12">
                         {playersTeam}
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <h1 className="card-title">{strTeam}</h1>
+                        <h4 className="card-text"> <BiWorld style={{ height: 25, width: 25 }} /> Founded: {intFormedYear}</h4>
+                        <h4 className="card-text"> <FiFlag style={{ height: 25, width: 25 }} /> Country: {strCountry}</h4>
+                        <h4 className="card-text"><RiFootballLine style={{ height: 25, width: 25 }} /> Sport Type: Football</h4>
+                        <h4 className="card-text"><CgGenderMale style={{ height: 50, width: 50 }} /> Gender: {strGender}</h4>
+                        <Link className="btn btn-dark" to={"/home"}><FontAwesomeIcon icon={faArrowLeft} /> Back </Link>
                     </div>
                 </div>
             </div>
